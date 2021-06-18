0a1b441ca550f641ae995c235ed23dd3a2fdcca7
Gabi <bibis-goulart@hotmail.com>
2021-06-18 10:31:55 -0300

    Alteracoes aula 2

---
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem Somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+            debitis voluptatibus libero. Voluptate adipisci quaerat vero,
+            officia esse fuga dignissimos vitae doloremque, deleniti natus neque
+            aspernatur cupiditate iure? Sapiente, velit?
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"AppStore"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
